27666b385a6f876e0e67c46dafb2bf42459d100b
Kunal Gathe <kunalgathe121@example.com>
2023-11-29 15:14:58 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 
-let heading = React.createElement("h1", { key: "2016" }, "hello");
-let heading2 = React.createElement("h2", { key: "2015" }, "World");
 
-let divTag = React.createElement(
-  "div",
-  {
-    style: { backgroundColor: "red" },
-  },
-  [heading, heading2]
-);
 
-let root = ReactDOM.createRoot(document.getElementById("root"));
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h1>World</h1>
+    </div>
+  );
+}
 
-root.render(divTag);
+export default App;
